be7276222baa7e7d7f63fc314051850d59c80c5a
qixinh <qixinh@jumei.com>
2016-06-22 18:39:47 +0800

    opt

--- app/src/main/java/com/xinran/qxdadian/FragmentsActivity.java
+++ app/src/main/java/com/xinran/qxdadian/FragmentsActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.xinran.qxdadian.base.QxBaseActivity;
+import com.xinran.qxdadian.base.QxBaseFragment;
 import com.xinran.qxdadian.base.QxBaseFragmentPagerAdapter;
 import com.xinran.qxdadian.base.QxBaseFragmentStatePagerAdapter;
 import com.xinran.qxdadian.fragments.FirstFragmentForViewPager;
@@ -32,13 +33,13 @@ public class FragmentsActivity extends QxBaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragments);
         mViewPager = $(R.id.vp_fragments);
-        List<Fragment> list = getFragments();
+        List<QxBaseFragment> list = getFragments();
         mViewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager(), list));
         mViewPager.setOffscreenPageLimit(0);
     }
 
-    private List<Fragment> getFragments() {
-        List<Fragment> list = new ArrayList<>();
+    private List<QxBaseFragment> getFragments() {
+        List<QxBaseFragment> list = new ArrayList<>();
         list.add(new FirstFragmentForViewPager());
         list.add(new SencondFragmentForViewPager());
         list.add(new ThirdFragmentForViewPager());
@@ -46,61 +47,21 @@ public class FragmentsActivity extends QxBaseActivity {
         return list == null ? Collections.EMPTY_LIST : list;
     }
 
-    private static class ViewPagerAdapter extends QxBaseFragmentPagerAdapter {
+    private static class ViewPagerAdapter extends QxBaseFragmentPagerAdapter<QxBaseFragment> {
 
-
-        List<Fragment> list;
-
-        public ViewPagerAdapter(FragmentManager fm, List<Fragment> list) {
-            super(fm);
-            this.list = list;
-        }
-
-        @Override
-        public Fragment getItem(int i) {
-
-
-            return list == null ? null : list.get(i);
+        public ViewPagerAdapter(FragmentManager fm, List<QxBaseFragment> list) {
+            super(fm,list);
         }
 
 
-        @Override
-        public int getCount() {
-            return list == null ? 0 : list.size();
-        }
     }
 
-    private static class ViewStatePagerAdapter extends QxBaseFragmentStatePagerAdapter {
-        List<Fragment> list;
+    private static class ViewStatePagerAdapter extends QxBaseFragmentStatePagerAdapter <QxBaseFragment> {
 
-        public ViewStatePagerAdapter(FragmentManager fm, List<Fragment> list) {
-            super(fm);
-            this.list = list;
+        public ViewStatePagerAdapter(FragmentManager fm, List<QxBaseFragment> list) {
+            super(fm,list);
         }
 
-        @Override
-        public Fragment getItem(int i) {
-
-
-            return list == null ? null : list.get(i);
-        }
-
-
-        @Override
-        public int getCount() {
-            return list == null ? 0 : list.size();
-        }
-
-        @Override
-        public int getItemPosition(Object object) {
-            return PagerAdapter.POSITION_NONE;
-        }
-
-        @Override
-        public void destroyItem(View container, int position, Object object) {
-
-            super.destroyItem(container, position, object);
-        }
     }
 
 
--- app/src/main/java/com/xinran/qxdadian/base/QxBaseFragmentPagerAdapter.java
+++ app/src/main/java/com/xinran/qxdadian/base/QxBaseFragmentPagerAdapter.java
@@ -1,32 +1,54 @@
 package com.xinran.qxdadian.base;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.util.Log;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 
 /**
  * Created by qixinh on 16/6/22.
  */
-public abstract class QxBaseFragmentPagerAdapter extends FragmentPagerAdapter {
+public class QxBaseFragmentPagerAdapter<E extends Fragment> extends FragmentPagerAdapter {
 
-    protected int cur = -1;
+    private int cur = -1;
+    protected List<E> mList;
 
-    public QxBaseFragmentPagerAdapter(FragmentManager fm) {
+    public QxBaseFragmentPagerAdapter(FragmentManager fm, List<E> list) {
         super(fm);
+        this.mList = list;
     }
 
+    protected void setData(List<E> list) {
+        mList.addAll(list);
+        notifyDataSetChanged();
+    }
 
     @Override
     public void setPrimaryItem(ViewGroup container, int position, Object object) {
         super.setPrimaryItem(container, position, object);
-        if (cur != position) {
-            Log.e("NB", "position" + position);
+        if (cur != position) {//因为会连续重复多次
+            if (cur != -1) {
+                Log.e("NB", "Pre_position:==" + cur + " ClassName:==" + mList.get(cur).getClass().getSimpleName());
+            }
+            Log.e("NB", "Cur_position:==" + position + " ClassName:==" + object.getClass().getSimpleName());
             cur = position;
         }
 
     }
 
+    @Override
+    public E getItem(int i) {
+        return mList == null ? null : mList.get(i);
+    }
+
+
+    @Override
+    public int getCount() {
+        return mList == null ? 0 : mList.size();
+    }
 }
